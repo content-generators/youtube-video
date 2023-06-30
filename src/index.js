f56631ce5bd89adb3cdda0a7d84c952c92be0bf5
Sanchit Gupta <gupta.sanchit90@gmail.com>
2023-06-30 18:26:41 +0530

    Asset path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {CONTEXT} from "@content-generators/ui-components";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CONTEXT.UiComponentContext.Provider value={{
-    staticFilePath: "/assets" 
+    staticFilePath: "assets" 
   }}>
     {" "}
     <App />
