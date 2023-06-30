d110767963034c372437ac295e3e4390b0e21172
Sanchit Gupta <gupta.sanchit90@gmail.com>
2023-06-30 15:58:08 +0530

    Dynamic content

--- src/App.js
+++ src/App.js
@@ -3,37 +3,18 @@ import { VIDEO, BITS, VIDEO_INTRO } from "@content-generators/ui-components";
 import { useRef } from "react";
 
 function App() {
-  const [data, setData] = useState({
-    "title": "Tablet Video",
-    "component": "MobileDetails",
-    "intro":"02",
-    "pages": [
-      {
-        "title": "Apple iPad Air 5th Gen 5G Tablet (256GB)",
-        "image": "https://cdn1.smartprix.com/rx-iM3VA3pOH-w420-h420/apple-ipad-air-5th-g.webp",
-        "points": [
-          "A sleek and powerful tablet with a 10.9-inch Liquid Retina IPS LCD display and a 12 MP rear camera that can record 4K videos.",
-          "A single SIM device that supports 5G, VoLTE, wifi-hotspot, USB-C and USB on-the-go features for fast and seamless connectivity.",
-          "A side-mounted fingerprint sensor and face unlock for enhanced security and convenience.",
-          "An octa-core Apple M1 processor and an Apple GPU (8-core graphics) for smooth performance and stunning graphics.",
-          "A 256 GB internal storage that can store all your photos, videos, music and documents without a card slot.",
-          "A non-removable Li-Po battery that can last up to 10 hours of talk time and supports 20W fast charging with a USB-C power adapter.",
-          "An iOS v15 operating system that offers a user-friendly interface and access to a variety of apps and services."
-        ]
-      }
-    ]
-  });
+  const [data, setData] = useState(null);
   const [pageNum, setPageNum] = useState(0);
   const [showView, setShowView] = useState("INTRO");
   let vidRef = useRef();
-
+  const textRef = useRef(null);
 
   const getComponent = (data) => {
     if (!data) {
       return null;
     }
-    if(showView!=="CONTENT"){
-      return null
+    if (showView !== "CONTENT") {
+      return null;
     }
     return React.createElement(VIDEO[data.component], {
       ...data.pages[pageNum],
@@ -51,14 +32,14 @@ function App() {
     if (!data) {
       return null;
     }
-    if(showView!=="INTRO"){
-      return null
+    if (showView !== "INTRO") {
+      return null;
     }
     return React.createElement(VIDEO_INTRO[`Intro_${data.intro}`], {
       ...data,
-      videoSrc: "videos/artifacts_-_23761 (1080p).mp4",
+      videoSrc: process.env.PUBLIC_URL + "videos/artifacts_-_23761 (1080p).mp4",
       handleEvent: () => {
-        // setShowView("CONTENT")
+        setShowView("CONTENT")
       },
     });
   };
@@ -72,12 +53,30 @@ function App() {
     };
   }, []);
 
-  return (
+  return data ? (
     <>
       <BITS.VideoRecorder recorderRef={vidRef} waitAfter={2} />
       {getIntro(data)}
       {getComponent(data)}
     </>
+  ) : (
+    <div className="p-8">
+      <textarea
+        ref={textRef}
+        id="message"
+        rows="20"
+        className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      ></textarea>
+      <button
+        type="button"
+        onClick={() => {
+          setData(JSON.parse(textRef.current.value));
+        }}
+        className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </div>
   );
 }
 
