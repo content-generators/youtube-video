0e18804e35daa02c46b1722642304acf84760383
Sanchit Gupta <gupta.sanchit90@gmail.com>
2023-06-30 18:23:33 +0530

    Asset path fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {CONTEXT} from "@content-generators/ui-components";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CONTEXT.UiComponentContext.Provider value={{
-    staticFilePath: process.env.NODE_ENV === 'development' ? "/assets" : "/public/assets" 
+    staticFilePath: "/assets" 
   }}>
     {" "}
     <App />
