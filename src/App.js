4063b2ddda4fb9414341827e5f96d444f7c2ca69
Sanchit Gupta <gupta.sanchit90@gmail.com>
2023-07-02 18:08:01 +0530

    Video Data form

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { VIDEO, BITS, VIDEO_INTRO } from "@content-generators/ui-components";
+import { VIDEO, BITS } from "@content-generators/ui-components";
 import { useRef } from "react";
+import DataBuilderComponent from "./DataBuilderComponent";
 
 function App() {
   const [data, setData] = useState(null);
   const [pageNum, setPageNum] = useState(0);
-  const [showView, setShowView] = useState("INTRO");
   let vidRef = useRef();
-  const textRef = useRef(null);
 
   const getComponent = (data) => {
     if (!data) {
       return null;
     }
-    if (showView !== "CONTENT") {
-      return null;
-    }
-    return React.createElement(VIDEO[data.component], {
+    return React.createElement(VIDEO[data.pages[pageNum].component], {
       ...data.pages[pageNum],
       handleEvent: () => {
         if (pageNum < data.pages.length - 1) {
           setPageNum(pageNum + 1);
         } else {
-          vidRef.current.stopRecording();
+          if (vidRef && vidRef.current) {
+            vidRef.current.stopRecording();
+          }
         }
-      },
-    });
-  };
-
-  const getIntro = (data) => {
-    if (!data) {
-      return null;
-    }
-    if (showView !== "INTRO") {
-      return null;
-    }
-    return React.createElement(VIDEO_INTRO[`Intro_${data.intro}`], {
-      ...data,
-      videoSrc: "assets/videos/artifacts_-_23761 (1080p).mp4",
-      handleEvent: () => {
-        setShowView("CONTENT")
       },
     });
   };
@@ -56,27 +38,14 @@ function App() {
   return data ? (
     <>
       <BITS.VideoRecorder recorderRef={vidRef} waitAfter={2} />
-      {getIntro(data)}
       {getComponent(data)}
     </>
   ) : (
-    <div className="p-8">
-      <textarea
-        ref={textRef}
-        id="message"
-        rows="20"
-        className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      ></textarea>
-      <button
-        type="button"
-        onClick={() => {
-          setData(JSON.parse(textRef.current.value));
-        }}
-        className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
-    </div>
+    <DataBuilderComponent
+      handleData={(data) => {
+        setData(data);
+      }}
+    />
   );
 }
 
---
+++ src/DataBuilderComponent.jsx
@@ -0,0 +1,67 @@
+import { VIDEO } from "@content-generators/ui-components";
+import { useState, useEffect } from "react";
+
+const DataBuilderComponent = ({ handleData }) => {
+  const [data, setData] = useState({});
+
+  return (
+    <div className="p-8">
+      <div className="my-4">
+        <label htmlFor="video_title" className="block mb-2">
+          Video Title
+        </label>
+        <input
+          type="text"
+          value={data?.title}
+          onChange={(e) => {
+            setData({
+              ...data,
+              title: e.target.value,
+            });
+          }}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Video Title"
+          required
+        />
+      </div>
+
+      <div className="my-4">
+        <label htmlFor="video_pages" className="block mb-2">
+          Video Pages
+        </label>
+        <textarea
+          value={data.pages}
+          rows="15"
+          onChange={(event) => {
+            try {
+              console.log(JSON.parse(event.target.value));
+              setData({
+                ...data,
+                pages: event.target.value,
+              });
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        ></textarea>
+      </div>
+
+      <button
+        onClick={() => {
+          // submit()
+          const jsonData = {
+            ...data,
+            pages: JSON.parse(data.pages),
+          };
+          handleData(jsonData);
+        }}
+        className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default DataBuilderComponent;
