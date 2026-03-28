490d8b835898f0fca2ae9b9086645fb042ee307c
Sanchit Gupta <gupta.sanchit90@gmail.com>
2026-03-28 15:19:09 +0530

    Using Vite, removed react-scripts

---
+++ src/main.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CONTEXT } from "@content-generators/ui-components";
+import { createRoot } from "react-dom/client";
+import _ from 'underscore';
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const isTtsServiceHealthy = async () => {
+  try {
+    const healthCheck = await fetch(`${import.meta.env.VITE_VOICE_GENERATOR}/health-check`);
+    const healthStatus = await healthCheck.json();
+    return healthStatus && healthStatus.status === "ok";
+  } catch (e) {
+    console.error("TTS Service Health Check Failed", e);
+    return false;
+  }
+
+};
+
+const initApp = async () => {
+  const isHealthy = await isTtsServiceHealthy();
+  
+  root.render(
+    <CONTEXT.UiComponentContext.Provider value={{
+      staticFilePath: "assets",
+      tts_url_buillder: import.meta.env.VITE_VOICE_GENERATOR && isHealthy ? (text, voice) => {
+        console.log(_.unescape(text));
+        console.log(import.meta.env.MODE);
+        console.log(import.meta.env.VITE_VOICE_GENERATOR);
+
+        const ttsEngine = window.tts_engine || 'piper'
+        const ttsVoice = window.tts_voice || voice
+
+        return `${import.meta.env.VITE_VOICE_GENERATOR}/generate?engine=${ttsEngine}&voice=${ttsVoice}&text=${unEscape(_.unescape(text))}`
+
+      } : () => {
+        console.error("Unable to load TTS Service");
+        return null
+      }
+    }}>
+      {" "}
+      <App />
+    </CONTEXT.UiComponentContext.Provider>
+  );
+};
+
+initApp();
+
+function unEscape(htmlStr) {
+  htmlStr = htmlStr.replace(/&lt;/g, "<");
+  htmlStr = htmlStr.replace(/&gt;/g, ">");
+  htmlStr = htmlStr.replace(/&quot;/g, "\"");
+  htmlStr = htmlStr.replace(/&#39;/g, "\'");
+  htmlStr = htmlStr.replace(/&#039;/g, "\'");
+  htmlStr = htmlStr.replace(/&amp;/g, "&");
+  return htmlStr;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@content-generators/ui-components/dist/**/*.js"
